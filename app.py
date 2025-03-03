cd7084c32eba10b7f6de9f048d55db75fc390f0a
Ryota14-git <m-ryota0814@ezweb.ne.jp>
2025-03-03 19:40:46 +0900

    update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import os
+import re
 
 from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from langchain.chat_models import ChatOpenAI    
+
 
 load_dotenv()
 
@@ -12,9 +15,16 @@ app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
 @app.event("app_mention")
 def handle_mention(event, say):
-    user = event["user"]
     thread_ts = event["ts"]
-    say(thread_ts=thread_ts,text=f"Hello <@{user}>!")
+    message = re.sub("<@.*>","",event["text"])
+
+    llm=ChatOpenAI(
+        model_name=os.environ["OPENAI_API_MODEL"],
+        temperature=os.environ["OPENAI_API_TEMPERATURE"],
+    )
+
+    response=llm.predict(message)
+    say(text=response, thread_ts=thread_ts)
 
 
 # ソケットモードハンドラーを使ってアプリを起動します
